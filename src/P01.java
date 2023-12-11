935878e594b73eec8f819099fd6724104e7d127c
eliffeates <elif.ates.1029@gmail.com>
2023-12-11 14:39:17 +0300

    2.GunIlk version

---
+++ src/P01.java
@@ -0,0 +1,12 @@
+public class P01 {
+
+    public static void main(String[] args) {
+
+        int a= 128;
+        int b=256;
+
+        System.out.println("İki sayının toplamı:" + (a + b));
+    }
+}
+
+
